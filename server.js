13a2ad26991ccc71b690f5bc521b44965e3ad7ec
gonzzoh <gmeza0118@gmail.com>
2021-07-29 21:49:48 -0400

    Commit 2: Tip Calculator express application created.

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ app.listen(port, () => {
     console.log(`your app is running on port: ${port}`)
 })
 
-app.get('/greeting', (req, res) => {
-    res.send(`wadditdeww`)
-});
+// app.get('/greeting', (req, res) => {
+//     res.send(`wadditdeww`)
+// });
 
-app.get('/greeting/:name', (req, res) => {
-    const name = req.params.name
-    res.send(`wassup ${name}`)
+// app.get('/greeting/:name', (req, res) => {
+//     const name = req.params.name
+//     res.send(`wassup ${name}`)
+// })
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let tipTotal = (req.params.tipPercentage/req.params.total) * 100
+    res.send(`The total tip will be: ${tipTotal}`)
 })
